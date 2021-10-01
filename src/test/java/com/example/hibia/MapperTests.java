f7c03338b5ae0dd248d48170b85f8d27e6a07bc7
ELBO0527 <63278770+ELBO0527@users.noreply.github.com>
2021-10-01 18:59:08 +0900

    1

---
+++ src/test/java/com/example/hibia/MapperTests.java
@@ -0,0 +1,27 @@
+package com.example.hibia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.hibia.configuration.domain.BoardDTO;
+import com.example.hibia.configuration.mapper.BoardMapper;
+
+@SpringBootTest
+class MapperTests {
+
+	@Autowired
+	private BoardMapper boardMapper;
+
+	@Test
+	public void testOfInsert() {
+		BoardDTO params = new BoardDTO();
+		params.setTitle("1번 게시글 제목");
+		params.setContent("1번 게시글 내용");
+		params.setWriter("테스터");
+
+		int result = boardMapper.insertBoard(params);
+		System.out.println("결과는 " + result + "입니다.");
+	}
+
+}
