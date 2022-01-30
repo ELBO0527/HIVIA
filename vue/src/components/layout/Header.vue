33c54fc76399da72503b6098cd70aef38ef1097a
ELBO0527 <sbwar@naver.com>
2022-01-30 15:00:59 +0900

    ff

---
+++ vue/src/components/layout/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-card class="overflow-hidden">
+    <!--앱바-->
+    <v-app-bar color="white">
+      <v-spacer></v-spacer>
+
+      <v-btn text color="black" dark v-bind="attrs" v-on="on" to="/">
+        Ksolution
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <!-- 회사소개 -->
+      <v-btn text color="primary" dark v-bind="attrs" v-on="on" to="/company">
+        회사소개
+      </v-btn>
+
+      <!-- 첫번째 드롭박스 -->
+      <div class="text-center">
+        <v-menu open-on-hover offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              text
+              color="primary"
+              dark
+              v-bind="attrs"
+              v-on="on"
+              to="/solution"
+            >
+              솔루션 검색
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, index) in items"
+              :key="index"
+              to="/solution"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+
+      <!-- 상품버튼 -->
+      <v-btn text color="primary" dark v-bind="attrs" v-on="on" to="/help">
+        문의하기
+      </v-btn>
+
+      <!--검색바-->
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-text-field
+        label="검색"
+        placeholder="검색"
+        solo
+        dense
+        prepend-inner-icon="mdi-magnify"
+        hide-details
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <!-- 아이콘-->
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn class="ma-2" color="primary" outlined to="/signin">로그인</v-btn>
+      <v-btn color="primary" dark v-bind="attrs" v-on="on" to="/signup"
+        >회원가입</v-btn
+      >
+    </v-app-bar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: {
+    modal: false,
+  },
+};
+</script>
