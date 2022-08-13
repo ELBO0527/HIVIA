e4a8d240557da99d2fbd10a476c44134679d02cb
ELBO0527 <63278770+ELBO0527@users.noreply.github.com>
2022-08-14 00:25:39 +0900

    ui

---
+++ vue/src/views/users/mypage/mypage-detail.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container
+  class="pa-6 ma-2">
+    <div>
+    <v-row cols="6" md="6" sm="6">
+      <v-col cols="12" md="6" sm="12">
+        <h1>상품 상세 페이지</h1>
+        <h1>상품 상세 페이지</h1>
+        <h1>상품 상세 페이지</h1>
+        <h1>상품 상세 페이지</h1>
+        <h1>상품 상세 페이지</h1>
+        <h1>상품 상세 페이지</h1>
+        <h1>상품 상세 페이지</h1>
+        <h1>상품 상세 페이지</h1>
+        <h1>상품 상세 페이지</h1>
+      </v-col>
+      <v-col cols="6" md="6" sm="6">
+        <h1>t2nter</h1>
+        <v-col cols="12" md="6" sm="6">
+        <v-btn
+        class="ma-2"
+        outlined
+        color="indigo">
+        장바구니에 담기
+        </v-btn>
+        <v-btn
+        class="ma-2"
+        outlined
+        color="indigo">
+        장바구니에 담기
+        </v-btn>
+        </v-col>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col col="12" md="12" sm="12">
+        <hr>
+      </v-col>
+      <v-col>
+
+      </v-col>
+    </v-row>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
