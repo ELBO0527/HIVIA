512f6b74efc99dcfcfd16e7f2ef61e8b2b219c7c
ELBO0527 <63278770+ELBO0527@users.noreply.github.com>
2022-10-02 02:12:53 +0900

    backjoon

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -6,43 +6,67 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static boolean checked[];
+    static int intArr[][];
+    static int node, line, start;
+    static Queue<Integer> queue = new LinkedList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        LinkedHashSet<String> str = new LinkedHashSet<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(input);
+        //strings[0] = 정점의 수 (<1000), strings[1] = 간선의수 (<10000), strings[2] = 탐색을 시작할 정점 번호
+        String[] inputArr = input.split(" ");
 
-        for (int i = 0; st.hasMoreTokens() == true ; i++){
-            String s = st.nextToken("[,]");
+        node = Integer.parseInt(inputArr[0]);
+        line = Integer.parseInt(inputArr[1]);
+        start = Integer.parseInt(inputArr[2]);
 
-            if (s.trim().contains(" ")){
-            }else{
-                str.add(s.trim());
-                arr.add(str.size());
+        intArr = new int[node+1][node+1];
+        checked = new boolean[node+1];
+
+        for (int i=0; i< line; i++){
+            String linked = br.readLine();
+            String[] lineArr = linked.split(" ");
+
+            int firstNode = Integer.parseInt(lineArr[0]);
+            int secondNode = Integer.parseInt(lineArr[1]);
+
+            intArr[firstNode][secondNode] = intArr[secondNode][firstNode] = 1;
+        }
+
+        dfs(start);
+
+        checked = new boolean[node+1];
+
+        bfs(start);
+
+    }
+
+    public static void dfs(int start){
+        check[start] = true;
+        sb.append(start + " ");
+
+        for(int i = 0 ; i <= node ; i++) {
+            if(arr[start][i] == 1 && !check[i])
+                dfs(i);
+        }
+    }
+
+    public static void bfs(int start){
+        q.add(start);
+        check[start] = true;
+
+        while(!q.isEmpty()) {
+
+            start = q.poll();
+            sb.append(start + " ");
+
+            for(int i = 1 ; i <= node ; i++) {
+                if(arr[start][i] == 1 && !check[i]) {
+                    q.add(i);
+                    check[i] = true;
+                }
             }
         }
-
-        for(int a = 0; a<arr.size();a++){
-            if (!arr2.contains(arr.get(a))){
-                arr2.add(arr.get(a));
-            }
-        }
-        String[] stringList = new String[str.size()];
-
-        int i = 0;
-        Iterator<String> iter = str.iterator();
-        while(iter.hasNext())
-        {
-            stringList[i] = iter.next();
-            i++;
-        }
-
-        for (int j = 0; j< str.size(); j++){
-            System.out.printf("[%d] %s\n", arr2.get(j), stringList[j] );
-        }
-
-
     }
 }
